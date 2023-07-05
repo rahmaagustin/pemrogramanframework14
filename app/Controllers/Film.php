13f79d2d3314c61b2abdd4252295d9f92d197293
rahmaagustin <rahmaagustin1408@gmail.com>
2023-07-06 02:18:12 +0700

    Update dan Delete ke Database

--- app/Controllers/Film.php
+++ app/Controllers/Film.php
@@ -6,26 +6,178 @@ use App\Controllers\BaseController;
 
 // step 1
 use App\Models\FilmModel;
+use App\Models\GenreModel; //tambahan(1)
 
 class Film extends BaseController
 {
     //step 2
     protected $film;
+    protected $genre; //tambahan(2)
     //step 3 buat fungsi construct untuk inisasi class model
     public function __construct()
     {
         //step 4 
         $this->film = new FilmModel();
+        $this->genre = new GenreModel(); //tambahan(3)
     }
 
     public function index()
     {
-        $data['data_film'] = ($this->film->getFilm());
+        $data['data_film'] = ($this->film->getAllDataJoin());
         return view("film/index", $data);
     }
     
-    public function all(){
-        $data['semuafilm'] = $this->film->getallData();
+    public function add(){
+        $data["genre"] = $this->genre->getAllData();
+        $data["errors"] = session('errors');
+        return view("film/add", $data);
+    }
+
+    public function store(){
+        $validation = $this->validate([
+            'nama_film' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Nama Film Harus diisi'
+                ]
+            ],
+            'id_genre' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Genre Harus diisi'
+                ]
+            ],
+            'duration' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Durasi Harus diisi'
+                ]
+            ],
+            'cover' => [
+                'rules' => 'uploaded[cover]|mime_in[cover,/image/jpg,image/jpeg,image/png]|max_size[cover,2048]',
+                'errors' => [
+                    'uploaded' => 'Kolom Cover Harus berisi file.',
+                    'mime_size' => 'Tipe file pada kolom cover harus berupa JPG, JPEG, atau PNG',
+                    'max_size' => 'Ukuran file pada kolom Cover melebihi batas maksimum'
+                ]
+            ]
+
+        ]);
+
+        if (!$validation){
+            $errors = \Config\Services::validation()->getErrors();
+
+            return redirect()->back()->withInput()->with('errors', $errors);
+        }
+        $image = $this->request->getFile('cover');
+        //generate nama file yang unik
+        $imageName = $image->getRandomName();
+        //pindahkan file ke direktori penyimpanan
+        $image->move(ROOTPATH . 'public/assets/cover', $imageName);
+
+        $data = [
+            'nama_film'=> $this->request->getPost('nama_film'),
+            'id_genre'=> $this->request->getPost('id_genre'),
+            'duration'=> $this->request->getPost('duration'),
+            'cover' => $imageName,
+        ];
+
+        $this->film->save($data);
+        session()->setflashdata('success', 'Data berhasil disimpan.'); //tambahkan ini
+        return redirect()->to('/film');
+    }
+
+    public function update($id)
+    {
+        $decryptedId = decryptUrl($id);
+        $data["genre"] = $this->genre->getAllData();
+        $data["errors"] = session('errors');
+        $data["film"] = $this->film->getDataByID($decryptedId);
+        return view("film/edit", $data);
+    }
+
+    public function edit(){
+        $validation = $this->validate([
+            'nama_film' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Nama Film Harus diisi'
+                ]
+            ],
+            'id_genre' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Genre Harus diisi'
+                ]
+            ],
+            'duration' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kolom Durasi Harus diisi'
+                ]
+            ],
+            //ubah rulesnya
+            'cover' => [
+                'rules' => 'mime_in[cover,image/jpg,image/jpeg,image/png]|max_size[cover,2048]',
+                'errors' => [
+                    'mime_in' => 'Tipe file pada kolom cover harus berupa JPG, JPEG, atau PNG',
+                    'max_size' => 'Ukuran file pada kolom Cover melebihi batas maksimum'
+                ]
+            ]
+
+        ]);
+
+        if (!$validation){
+            $errors = \Config\Services::validation()->getErrors();
+
+            return redirect()->back()->withInput()->with('errors', $errors);
+        }
+
+        // ambil data lama
+        $semuafilm = $this->film->find($this->request->getPost('id'));
+
+        // tambah request id
+        $data = [
+            'id'=> $this->request->getPost('id'),
+            'nama_film'=> $this->request->getPost('nama_film'),
+            'id_genre'=> $this->request->getPost('id_genre'),
+            'duration'=> $this->request->getPost('duration'),
+        ];
+
+        //cek apakah ada cover yang diupload
+        $cover = $this->request->getFile('cover');
+        if ($cover->isValid() && !$cover->hasMoved()){
+            // Generated nama file yang unik
+            $imageName = $cover->getRandomName();
+            //pindahkan file ke direktori penyimpanan
+            $image->move(ROOTPATH . 'public/assets/cover', $imageName);
+            // Hapus file gambar lama jika ada
+            if ($semuafilm['cover']) {
+                unlink(ROOTPATH . 'public/assets/cover'. $semuafilm['cover']);
+            }
+            // jika ada tambahkan array cover pada variable $data
+            $data['cover'] = $imageName;
+        }
+
+        $this->film->save($data);
+        
+        // ubah pesannya
+        session()->setflashdata('success', 'Data berhasil diperbarui.');
+        return redirect()->to('/film');
+    }    
+
+    public function destroy($id)
+    {
+        $decryptedId = decryptUrl($id);
+        $this->film->delete($decryptedId);
+        session()->setFlashdata('success', 'Data berhasil dihapus.');![a]('https://github.com/irfanltf/temankoding-ci4/assets/48278734/c50de6cb-d940-4119-b87c-50a08d258143');
+
+        return redirect()->to('/film');
+    }
+
+    public function all()
+    {
+        $data['semuafilm'] = ($this->film->getAllDataJoin());
         return view("film/semuafilm", $data);
     }
     
--- app/Views/film/index.php
+++ app/Views/film/index.php
@@ -1,30 +1,75 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DATA FILM</title>
-</head>
-<body>
-    <h1>Data Film</h1>
-    <table border="1" celllspacing="0" cellpadding="5">
+<?php $this->extend ("layout/layout") ?>
+<?php $this->section("content")?>
+
+<div class="container">
+
+<div class="row">
+<div class="col-md-12">
+  <div class ="card">
+
+    <div class="card-header">
+      <div class="row">
+        <div class="col-md-6">
+            <h1>Semua Film</h1>
+        </div>
+        <div class="col-md-6 text-end">
+            <a href="/film/add" class="btn btn-primary">Tambah Data</a>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+  <div class="card-body"></div>
+    <table class="table table-striped">
         <tr>
             <th>No</th>
             <th>Nama Film</th>
+            <th>Gambar</th>
             <th>Genre</th>
-            <th>Durasi</th>
+            <th>Duration</th>
+            <th>Action</th>
         </tr>
-        <?php $i = 1; ?>
-        <?php foreach($data_film as $row): ?>
+        <?php $i = 1;?>
+        <?php foreach ($data_film as $film) :  ?> 
             <tr>
                 <td><?= $i++; ?></td>
-                <td><?php echo $row['nama_film']?></td>
-                <td><?php echo $row['genre']?></td>
-                <td><?php echo $row['duration']?></td>
+                <td><?= $film["nama_film"] ?></td>
+                <td><img src="/assets/cover/<?= $film["cover"] ?>" class="card-img-top full-width-image"
+                alt="Poster Film" style="width:50px" ></td>
+                <td><?= $film["nama_genre"] ?></td>
+                <td><?= $film["duration"] ?></td>
+                <td>
+                    <a href="/film/update/ <?= encryptUrl($film["id"]); ?>" class="btn btn-success">Update</a>
+                    <a class="btn btn-warning"onclick="return confirmDelete()">Delete</a>
+                </td>
             </tr>
         <?php endforeach; ?>
+        </table>        
+</div>    
+</div>
+</div>
 
+<!-- tambahkan dari sini  -->
+<script>
+    function confirmDelete() {
+        swal({
+                title: "Apakah Anda yakin?",
+                text: "setelah dihapus! data anda akan benar-benar hilang!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
 
-    </table>
-</body>
-</html>
+                    window.location.href = "/film/destroy/<?= encryptUrl($film['id']) ?>";
+
+                } else {
+                    swal("Data tidak jadi dihapus!");
+                }
+            });
+    }
+</script>
+  <!-- sampai sini -->
+
+<?php $this->endsection();
